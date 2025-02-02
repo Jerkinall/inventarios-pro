bdae68a4fcbe94f3776e209566a90d5242fb6cb3
Jerkinall <Jerkinall@gmail.com>
2025-02-02 19:17:08 -0400

    se elimino el contenido de la capeta components

--- src/components/PrimaryComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const PrimaryComponent = () => {
-  return (
-    <div>PrimaryComponent</div>
-  )
-}
--- src/components/SecondaryComponent.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const SecondaryComponent = () => {
-  return (
-    <div>SecondaryComponent</div>
-  )
-}
